965f9955ecc31078aa86576b0428220206bc52ca
santawho <196087559+santawho@users.noreply.github.com>
2025-11-17 13:16:23 +0100

    get location suggestions from favorite stations

--- oeffi/src/de/schildbach/oeffi/util/locationview/LocationSuggestionsCollector.java
+++ oeffi/src/de/schildbach/oeffi/util/locationview/LocationSuggestionsCollector.java
@@ -16,6 +16,7 @@ import de.schildbach.oeffi.Constants;
 import de.schildbach.oeffi.directions.QueryHistoryProvider;
 import de.schildbach.oeffi.network.LocationSearchProviderFactory;
 import de.schildbach.oeffi.network.NetworkProviderFactory;
+import de.schildbach.oeffi.stations.FavoriteStationsProvider;
 import de.schildbach.pte.LocationSearchProvider;
 import de.schildbach.pte.LocationSearchProviderId;
 import de.schildbach.pte.NetworkId;
@@ -28,14 +29,14 @@ public class LocationSuggestionsCollector {
     private static final Logger log = LoggerFactory.getLogger(LocationSuggestionsCollector.class);
 
     public static List<Location> collectSuggestions(
-            final CharSequence constraint,
+            final CharSequence aConstraint,
             final AbstractSet<LocationType> suggestedLocationTypes,
             final NetworkId network,
             final LocationSearchProviderId searchProviderId) {
-        if (constraint == null)
+        if (aConstraint == null)
             return null;
-        final String constraintStr = constraint.toString().trim();
-        if (constraintStr.isEmpty())
+        final String constraint = aConstraint.toString().trim();
+        if (constraint.isEmpty())
             return null;
         if (suggestedLocationTypes.isEmpty()) {
             suggestedLocationTypes.add(LocationType.STATION);
@@ -48,82 +49,12 @@ public class LocationSuggestionsCollector {
         try {
             final List<Location> results = new LinkedList<>();
 
-            // local autocomplete
-            final Cursor cursor = Application.getInstance().getContentResolver().query(
-                    QueryHistoryProvider.CONTENT_URI().buildUpon().appendPath(network.name())
-                            .appendQueryParameter(QueryHistoryProvider.QUERY_PARAM_Q, constraintStr)
-                            .build(),
-                    null, null, null, QueryHistoryProvider.KEY_LAST_QUERIED + " DESC");
-
-            if (cursor == null)
-                return results;
-
-            final int fromTypeC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_TYPE);
-            final int fromIdC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_ID);
-            final int fromLatC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_LAT);
-            final int fromLonC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_LON);
-            final int fromPlaceC = cursor
-                    .getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_PLACE);
-            final int fromNameC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_NAME);
-            final int toTypeC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_TYPE);
-            final int toIdC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_ID);
-            final int toLatC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_LAT);
-            final int toLonC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_LON);
-            final int toPlaceC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_PLACE);
-            final int toNameC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_NAME);
-
-            while (cursor.moveToNext()) {
-                final String fromName = cursor.getString(fromNameC);
-                if (fromName.toLowerCase(Constants.DEFAULT_LOCALE)
-                        .contains(constraintStr.toLowerCase(Constants.DEFAULT_LOCALE))) {
-                    final LocationType fromType = QueryHistoryProvider
-                            .convert(cursor.getInt(fromTypeC));
-                    final String fromId = cursor.getString(fromIdC);
-                    final int fromLat = cursor.getInt(fromLatC);
-                    final int fromLon = cursor.getInt(fromLonC);
-                    final Point fromCoord = fromLat != 0 || fromLon != 0
-                            ? Point.from1E6(fromLat, fromLon) : null;
-                    final String fromPlace = cursor.getString(fromPlaceC);
-                    final Location location = new Location(fromType, fromId, fromCoord, fromPlace,
-                            fromName);
-                    if (!results.contains(location))
-                        results.add(location);
-                }
-                final String toName = cursor.getString(toNameC);
-                if (toName.toLowerCase(Constants.DEFAULT_LOCALE)
-                        .contains(constraintStr.toLowerCase(Constants.DEFAULT_LOCALE))) {
-                    final LocationType toType = QueryHistoryProvider
-                            .convert(cursor.getInt(toTypeC));
-                    final String toId = cursor.getString(toIdC);
-                    final int toLat = cursor.getInt(toLatC);
-                    final int toLon = cursor.getInt(toLonC);
-                    final Point toCoord = toLat != 0 || toLon != 0 ? Point.from1E6(toLat, toLon)
-                            : null;
-                    final String toPlace = cursor.getString(toPlaceC);
-                    final Location location = new Location(toType, toId, toCoord, toPlace, toName);
-                    if (!results.contains(location))
-                        results.add(location);
-                }
-            }
-            cursor.close();
-
-            // remote autocomplete
-            final int constraintLength = constraint.length();
-            if (constraintLength >= 3) {
-                final int maxLocations = constraintLength >= 8 ? 15 : constraintLength * 2;
-                final LocationSearchProvider locationSearchProvider = searchProviderId != null
-                    ? LocationSearchProviderFactory.provider(searchProviderId)
-                    : NetworkProviderFactory.provider(network);
-                final SuggestLocationsResult suggestLocationsResult = locationSearchProvider
-                        .suggestLocations(constraint, suggestedLocationTypes, maxLocations);
-                if (suggestLocationsResult.status == SuggestLocationsResult.Status.OK) {
-                    final List<Location> foundLocations = suggestLocationsResult.getLocations();
-                    for (final Location location : foundLocations) {
-                        if (!results.contains(location))
-                            results.add(location);
-                    }
-                }
-            }
+            loadResultsFromFavoriteStations(constraint, network, results);
+            loadResultsFromQueryHistory(constraint, network, results);
+            loadResultsFromNetworkProvider(constraint,
+                    network, searchProviderId,
+                    suggestedLocationTypes,
+                    results);
 
             final List<Location> filteredResults = new ArrayList<>();
             for (final Location location : results) {
@@ -144,6 +75,135 @@ public class LocationSuggestionsCollector {
         } catch (final IOException ioe) {
             log.error("collectSuggestions failed", ioe);
             return null;
+        }
+    }
+
+    private static void loadResultsFromFavoriteStations(
+            final String constraint,
+            final NetworkId network,
+            final List<Location> results) {
+        final Cursor cursor = Application.getInstance().getContentResolver().query(
+                FavoriteStationsProvider.CONTENT_URI().buildUpon()
+                        .appendQueryParameter(FavoriteStationsProvider.KEY_TYPE, String.valueOf(FavoriteStationsProvider.TYPE_FAVORITE))
+                        .appendQueryParameter(FavoriteStationsProvider.KEY_STATION_NETWORK, network.name())
+                        .build(),
+                null, null, null, null);
+
+        if (cursor == null)
+            return;
+
+        final int typeC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_TYPE);
+        final int idC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_ID);
+        final int latC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_LAT);
+        final int lonC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_LON);
+        final int placeC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_PLACE);
+        final int nameC = cursor.getColumnIndexOrThrow(FavoriteStationsProvider.KEY_STATION_NAME);
+
+        final String lowerCaseConstraint = constraint.toLowerCase(Constants.DEFAULT_LOCALE);
+
+        while (cursor.moveToNext()) {
+            final String fromName = cursor.getString(nameC);
+            if (fromName.toLowerCase(Constants.DEFAULT_LOCALE).contains(lowerCaseConstraint)) {
+                final LocationType type = LocationType.valueOf(cursor.getString(typeC));
+                final String id = cursor.getString(idC);
+                final int lat = cursor.getInt(latC);
+                final int lon = cursor.getInt(lonC);
+                final Point coord = lat != 0 || lon != 0 ? Point.from1E6(lat, lon) : null;
+                final String place = cursor.getString(placeC);
+                final Location location = new Location(type, id, coord, place, fromName);
+                if (!results.contains(location))
+                    results.add(location);
+            }
+        }
+        cursor.close();
+    }
+
+    private static void loadResultsFromQueryHistory(
+            final String constraint,
+            final NetworkId network,
+            final List<Location> results) {
+        final Cursor cursor = Application.getInstance().getContentResolver().query(
+                QueryHistoryProvider.CONTENT_URI().buildUpon()
+                        .appendPath(network.name())
+                        .appendQueryParameter(QueryHistoryProvider.QUERY_PARAM_Q, constraint)
+                        .build(),
+                null, null, null, QueryHistoryProvider.KEY_LAST_QUERIED + " DESC");
+
+        if (cursor == null)
+            return;
+
+        final int fromTypeC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_TYPE);
+        final int fromIdC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_ID);
+        final int fromLatC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_LAT);
+        final int fromLonC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_LON);
+        final int fromPlaceC = cursor
+                .getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_PLACE);
+        final int fromNameC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_FROM_NAME);
+        final int toTypeC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_TYPE);
+        final int toIdC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_ID);
+        final int toLatC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_LAT);
+        final int toLonC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_LON);
+        final int toPlaceC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_PLACE);
+        final int toNameC = cursor.getColumnIndexOrThrow(QueryHistoryProvider.KEY_TO_NAME);
+
+        final String lowerCaseConstraint = constraint.toLowerCase(Constants.DEFAULT_LOCALE);
+
+        while (cursor.moveToNext()) {
+            final String fromName = cursor.getString(fromNameC);
+            if (fromName.toLowerCase(Constants.DEFAULT_LOCALE).contains(lowerCaseConstraint)) {
+                final LocationType fromType = QueryHistoryProvider
+                        .convert(cursor.getInt(fromTypeC));
+                final String fromId = cursor.getString(fromIdC);
+                final int fromLat = cursor.getInt(fromLatC);
+                final int fromLon = cursor.getInt(fromLonC);
+                final Point fromCoord = fromLat != 0 || fromLon != 0
+                        ? Point.from1E6(fromLat, fromLon) : null;
+                final String fromPlace = cursor.getString(fromPlaceC);
+                final Location location = new Location(fromType, fromId, fromCoord, fromPlace,
+                        fromName);
+                if (!results.contains(location))
+                    results.add(location);
+            }
+            final String toName = cursor.getString(toNameC);
+            if (toName.toLowerCase(Constants.DEFAULT_LOCALE).contains(lowerCaseConstraint)) {
+                final LocationType toType = QueryHistoryProvider
+                        .convert(cursor.getInt(toTypeC));
+                final String toId = cursor.getString(toIdC);
+                final int toLat = cursor.getInt(toLatC);
+                final int toLon = cursor.getInt(toLonC);
+                final Point toCoord = toLat != 0 || toLon != 0 ? Point.from1E6(toLat, toLon)
+                        : null;
+                final String toPlace = cursor.getString(toPlaceC);
+                final Location location = new Location(toType, toId, toCoord, toPlace, toName);
+                if (!results.contains(location))
+                    results.add(location);
+            }
+        }
+        cursor.close();
+    }
+
+    private static void loadResultsFromNetworkProvider(
+            final String constraint,
+            final NetworkId network,
+            final LocationSearchProviderId searchProviderId,
+            final AbstractSet<LocationType> suggestedLocationTypes,
+            final List<Location> results
+    ) throws IOException {
+        final int constraintLength = constraint.length();
+        if (constraintLength >= 3) {
+            final int maxLocations = constraintLength >= 8 ? 15 : constraintLength * 2;
+            final LocationSearchProvider locationSearchProvider = searchProviderId != null
+                    ? LocationSearchProviderFactory.provider(searchProviderId)
+                    : NetworkProviderFactory.provider(network);
+            final SuggestLocationsResult suggestLocationsResult = locationSearchProvider
+                    .suggestLocations(constraint, suggestedLocationTypes, maxLocations);
+            if (suggestLocationsResult.status == SuggestLocationsResult.Status.OK) {
+                final List<Location> foundLocations = suggestLocationsResult.getLocations();
+                for (final Location location : foundLocations) {
+                    if (!results.contains(location))
+                        results.add(location);
+                }
+            }
         }
     }
 }
